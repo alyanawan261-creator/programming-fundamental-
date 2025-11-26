e95007e7b517c3aa959adb6e242ac0c65d4a2061
alyanawan261-creator <alyanawan261@gmail.com>
2025-11-26 14:45:40 +0000

    Add files via upload

---
+++ Alyan awan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "***********************************************" << endl;
+    cout << "*  Name:           Alyan Awan                        *" << endl;
+    cout << "*  Roll Number:    19                          *" << endl;
+    cout << "*  Department:     Software Engineering        *" << endl;
+    cout << "*  Address:        Hattian Bala                           *" << endl;
+    cout << "*  Hobby:          Playing Cricket                           *" << endl;
+    cout << "*  Qualification:   FSc                          *" << endl;
+    cout << "*  SSC Marks:       569                          *" << endl;
+    cout << "*  HSSC Marks:      653                          *" << endl;
+    cout << "***********************************************" << endl;
+
+    return 0;
+}
+
---
+++ student ID Card.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"----------------------------------------------------\n";
+	cout<< "\tstudent ID Card\n";
+	cout<<"----------------------------------------------------\n";
+	cout<< "Name:\t\talyan\n";
+	cout<< "Roll No:\t19\n";
+	cout<< "Deperment:\tsoftware engineering\n";
+	cout<< "Session:\t2025-2029\n";
+	cout<<"----------------------------------------------------\n";
+	return 0;
+	
+}
